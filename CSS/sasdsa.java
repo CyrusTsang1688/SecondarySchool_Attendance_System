e79b79886e39fca13c64081c2d00d3abbd0b793d
CyrusT <73983294+CyrusTsang1688@users.noreply.github.com>
2021-11-28 02:34:01 +0800

    Add files via upload

---
+++ CSS/sasdsa.java
@@ -0,0 +1,261 @@
+var SATableData = {
+    "data": [
+    {
+    "account_number": "SA0001",
+    "name": "zzzzzn",
+    "ps": "5421",
+    
+    "creation_date": "2011/04/25"
+    
+    },
+    {
+    "account_number": "SA0002",
+    "name": "gg ho",
+    "ps": "336s",
+    
+    "creation_date":"2012/03/29"
+    
+    },
+    {
+    "account_number": "SA0003",
+    "name": "haha o",
+    "ps": "ssd2",
+    
+    "creation_date": "2008/11/28"
+    
+    },
+    {
+    "account_number": "SA0004",
+    "name": "tt good",
+    "ps": "68912",
+    
+    "creation_date": "2012/12/02"
+    
+    },
+    {
+    "account_number": "SA0005",
+    "name": "leaf chan",
+    "ps": "cpu11",
+    
+    "creation_date": "2011/01/25"
+    
+    }
+    ]
+    };
+    var TCTableData = {
+    
+    "data": [
+    {
+    "account_number": "TC0001",
+    "name": "Au Wai Lun",
+    "ps": "pw",
+    "phone_number": "92492094",
+    "management_class":"1a,2a",
+    
+    "creation_date": "2011/04/25"
+    
+    },
+    {
+    "account_number": "TC0002",
+    "name": "gg ho",
+    "ps": "336s",
+    "phone_number": "21768951",
+    "management_class":"1a,2a",
+    
+    "creation_date":"2012/03/29"
+    
+    },
+    {
+    "account_number": "TC0003",
+    "name": "haha o",
+    "ps": "ssd2",
+    "phone_number": "21768951",
+    "management_class":"1a,2a",
+    
+    "creation_date": "2008/11/28",
+    
+    },
+    {
+    "account_number": "TC0004",
+    "name": "tt good",
+    "ps": "68912",
+    "phone_number": "21768951",
+    "management_class":"1a,2a",
+    
+    "creation_date": "2012/12/02"
+    
+    },
+    {
+    "account_number": "TC0005",
+    "name": "leaf chan",
+    "ps": "cpu11",
+    "phone_number": "21768951",
+    "management_class":"1a,2a",
+    
+    "creation_date": "2011/01/25"
+    
+    }
+    ]
+    };
+    var SDTableData1819 = {
+    "data": [
+    {
+    "account_number": "SD0001",
+    "name": "yan k",
+    "ps": "sad4jl51",
+    "class": "4A",
+    "phone_number": "21312442",
+    "attendance_rate": "75%",
+    "days_absent": "3",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0002",
+    "name": "tsang fun",
+    "ps": "52jgik221",
+    "class": "4A",
+    "phone_number": "10541515",
+    "attendance_rate": "85%",
+    "days_absent": "2",
+    "sex": "female"
+    },
+    {
+    "account_number": "SD0003",
+    "name": "chung tsz",
+    "ps": "989hj4121",
+    "class": "4D",
+    "phone_number": "54151514",
+    "attendance_rate": "95%",
+    "days_absent": "1",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0004",
+    "name": "hi tsz",
+    "ps": "esfkh51",
+    "class": "4F",
+    "phone_number": "15856287",
+    "attendance_rate": "20%",
+    "days_absent": "15",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0005",
+    "name": "lee man",
+    "ps": "asdmopj2",
+    "class": "4C",
+    "phone_number": "65156155",
+    "attendance_rate": "55%",
+    "days_absent": "6",
+    "sex": "female"
+    }
+    ]
+    };
+    var SDTableData1920 = {
+    "data": [
+    {
+    "account_number": "SD0006",
+    "name": "yan Klein",
+    "ps": "sahjgd451",
+    "class": "5A",
+    "phone_number": "21312442",
+    "attendance_rate": "75%",
+    "days_absent": "3",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0007",
+    "name": "Baker fun",
+    "ps": "52jhk221",
+    "class": "5A",
+    "phone_number": "10541515",
+    "attendance_rate": "85%",
+    "days_absent": "2",
+    "sex": "female"
+    },
+    {
+    "account_number": "SD0008",
+    "name": "chung Patel",
+    "ps": "98vyi921",
+    "class": "5D",
+    "phone_number": "54151514",
+    "attendance_rate": "95%",
+    "days_absent": "1",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0009",
+    "name": "Dopey tsz",
+    "ps": "esf25151",
+    "class": "5F",
+    "phone_number": "15856287",
+    "attendance_rate": "20%",
+    "days_absent": "15",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0010",
+    "name": "lee Nala",
+    "ps": "ashjkd2",
+    "class": "5C",
+    "phone_number": "65156155",
+    "attendance_rate": "55%",
+    "days_absent": "6",
+    "sex": "female"
+    }
+    ]
+    };
+    var SDTableData2021 = {
+    "data": [
+    {
+    "account_number": "SD0011",
+    "name": "Grumpy k",
+    "ps": "saftd451",
+    "class": "6A",
+    "phone_number": "21312442",
+    "attendance_rate": "75%",
+    "days_absent": "3",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0012",
+    "name": "tsang Dopey",
+    "ps": "52245121",
+    "class": "6A",
+    "phone_number": "10541515",
+    "attendance_rate": "85%",
+    "days_absent": "2",
+    "sex": "female"
+    },
+    {
+    "account_number": "SD0013",
+    "name": "chung Bashful",
+    "ps": "9892411",
+    "class": "6D",
+    "phone_number": "54151514",
+    "attendance_rate": "95%",
+    "days_absent": "1",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0014",
+    "name": "Simba tsz",
+    "ps": "esf56641",
+    "class": "6F",
+    "phone_number": "15856287",
+    "attendance_rate": "20%",
+    "days_absent": "15",
+    "sex": "male"
+    },
+    {
+    "account_number": "SD0015",
+    "name": "Rafiki man",
+    "ps": "asd5142",
+    "class": "6C",
+    "phone_number": "65156155",
+    "attendance_rate": "55%",
+    "days_absent": "6",
+    "sex": "female"
+    }
+    ]
+    };
